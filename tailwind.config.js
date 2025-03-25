6c5422f1d669b7300f3a9aab59d182cd71b63db5
karimwo7 <kurbanovkarim61@gmail.com>
2025-03-25 19:57:38 +0500

    com-one

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/ @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src//*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+
+      'xs': '0px',
+      'md': '510px',
+      'lg': '1280px',
+
+  },
+  },
+  plugins: [],
+}
